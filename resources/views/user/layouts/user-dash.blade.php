20f9726e639c77cd860c123b966c6c34de01aae9
greyvin-george99v <georgethuo99@gmail.com>
2022-09-19 16:31:25 +0300

    Done

--- resources/views/user/layouts/user-dash.blade.php
+++ resources/views/user/layouts/user-dash.blade.php
@@ -49,7 +49,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Brand Logo -->
     <a href="{{ \URL::to('/')}}" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Your Site</span>
+      <span class="brand-text font-weight-light">Wheels On Bike</span>
     </a>
 
     <!-- Sidebar -->
