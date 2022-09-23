438f780604a9825013283db05bf380a815cb2529
greyvin-george99v <georgethuo99@gmail.com>
2022-09-23 19:10:25 +0300

    full

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('home');
+});
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
 
-Route::get('/', [HomeController::class, 'index'])->name('index');
+// Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::group(['prefix'=>'admin', 'middleware'=>['admin','auth']], function(){
     Route::get('/',[AdminController::class,'index'])->name('admin.dashboard');
